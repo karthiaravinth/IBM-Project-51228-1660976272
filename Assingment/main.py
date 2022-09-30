16bcf2b1990bfac65489da61731daae35d7ad8ba
MANIVASANTHAN <113498451+MANIVASANTHAN@users.noreply.github.com>
2022-09-30 11:55:45 +0530

    Resume website creation using python flask

---
+++ Assingment/main.py
@@ -0,0 +1,22 @@
+from urllib.request import urlopen
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/resume_template')
+def resume_template():
+    return render_template("resume_template.html")
+
+@app.route('/create')
+def create():
+    return "<h1> Sorry for the Inconvince the server are under maintain </h1><h4>Try again later...</h4>"
+
+@app.route('/view')
+def view():
+    return '''<img src="/static/img/resume1.png" style="width: 100%">'''
+
+app.run()
